e1a7e21eabab3171a8301cb5eed8df5ba039afbb
Alexandre Regalado <alexandreregalado@hotmail.com>
2022-12-11 21:39:57 +0000

    extra case

---
+++ meta3/meta3/ThereIsNoReturn.java
@@ -0,0 +1,59 @@
+class Ta_Puxado {
+    //TWD acabou :(
+    public static int HojeNaoDa(int id) {
+        id = 1 * -func(a);
+        id = 1 * +func(a);
+        id = 1 * -1;
+        id = 1 * +1;
+        id = 1 * -Integer.parseInt(id[1]);
+        id = 1 * +Integer.parseInt(id[1]);
+        id = 1 * -id.length;
+        id = 1 * +id.length;
+        id = 1 * -+1;
+        id = 1 * -+-+-+-1;
+        int i;
+        double d;
+        boolean b;
+        return i;
+        return d;
+        return b;
+        return i+i;
+        return i-i;
+        return i*i;
+        return i/i;
+        return i%1;
+        return i<<i;
+        return i>>i;
+        return i<i;
+        return i==1;
+        return i+i*i%i/i-i<i==i<<i>>i;
+        return Integer.parseInt(args[i]);
+        return Integer.parseInt(args[args]);
+        return args.length;
+        return i.length;
+        return HojeNaoDa(id);
+        return -HojeNaoDa(id);
+        return +HojeNaoDa(id);
+        return !HojeNaoDa(id);
+        return NaoTaFacil();
+        return -NaoTaFacil();
+        return +NaoTaFacil();
+        return !NaoTaFacil();
+        return MyBad(1);
+        return -MyBad(1);
+        return +MyBad(1);
+        return !MyBad(1);
+        return i = b;
+        return -i;
+        return +i;
+        return !i;
+    }
+    public static boolean NaoTaFacil(int id) {
+
+    }
+
+    public static double MyBad(int id) {
+
+    }
+}
+
